b4c194dbc70f6dc15cd7a5870ff9c241d8fae2f1
ilanrich <54144308+ilanrich@users.noreply.github.com>
2024-01-10 13:06:00 -0500

    Testing adding multiple panels to one page

--- pages/1_Test.py
+++ pages/1_Test.py
@@ -57,3 +57,32 @@ st.write(f"Total POI count across countries, including parking lots POI is <b>{t
 st.dataframe(latest_release_df_styled, use_container_width=True)
 st.write("Latest Release - Parking")
 st.dataframe(parking_lots_df_styled, use_container_width=True)
+
+
+top_30_df = (
+    read_from_gsheets("Countries")
+    .assign(**{
+        "Distinct brands": lambda df: df["Distinct brands"].astype(int),
+        "Total POI": lambda df: df["Total POI"].str.replace(",", "").astype(int),
+        "Branded POI": lambda df: df["Branded POI"].str.replace(",", "").astype(int),
+        "% Branded": lambda df: (df["Branded POI"] / df["Total POI"]) * 100
+    })
+    .query('iso_country_code != "US"')
+    .rename(columns={"iso_country_code": "Country Code", "country": "Country"})
+    [["Country Code", "Country", "Total POI", "Distinct brands", "% Branded"]]
+    .head(30)
+)
+
+top_30_df_styled = (
+    top_30_df.style
+    .apply(lambda x: ['background-color: #D7E8ED' if i%2==0 else '' for i in range(len(x))], axis=0)
+    .format({
+        "Total POI": "{:,.0f}",
+        "Distinct brands": "{:,.0f}",
+        "Branded POI": "{:,.0f}",
+        "% Branded": "{:.1f}%"
+    })
+)
+
+st.write("POI and Brand Counts - Top 30 Countries outside the US")
+st.dataframe(top_30_df_styled, use_container_width=True)
