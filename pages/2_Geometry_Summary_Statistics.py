43c75b6949fae102246ab8b9c46a0bac6c822ae1
ilanrich <54144308+ilanrich@users.noreply.github.com>
2024-01-11 15:48:22 -0500

    Add in last 12 months chart

--- pages/2_Geometry_Summary_Statistics.py
+++ pages/2_Geometry_Summary_Statistics.py
@@ -70,3 +70,50 @@ top_30_df_geometry_styled = (
 
 st.write("POI Counts - Top 30 Countries Outside the US")
 st.dataframe(top_30_df_geometry_styled, use_container_width=True)
+
+
+#### Last 12 Months Geometry ####
+
+global_places_df = read_from_gsheets("Global Places")
+global_places_df = global_places_df[["Release month", "Country", "POI with polygons"]]
+
+for i, value in enumerate(global_places_df['Release month']):
+    try:
+        global_places_df.loc[i, 'Release month'] = pd.to_datetime(value, format='%b %Y').strftime('%Y-%m')
+    except ValueError:
+        global_places_df.loc[i, 'Release month'] = pd.to_datetime(value, format='%B %Y').strftime('%Y-%m')
+
+start_date_str = (datetime.now() - timedelta(days=365)).strftime("%Y-%m")
+
+global_places_df["Release month"] = pd.to_datetime(global_places_df["Release month"])
+last_12_months_df = global_places_df[
+    (global_places_df["Release month"] >= start_date_str) & (global_places_df["Release month"] <= datetime.now()) &
+    (global_places_df["Country"] != "Grand Total")
+]
+last_12_months_df["Release month"] = last_12_months_df["Release month"].dt.strftime("%Y-%m")
+last_12_months_df['POI with polygons'] = pd.to_numeric(last_12_months_df['POI with polygons'])
+
+# st.dataframe(last_12_months_df)
+
+last_12_months_geometry = alt.Chart(last_12_months_df).mark_bar().encode(
+    x='Release month',
+    y='POI with polygons',
+    color='Country',
+    tooltip=[alt.Tooltip('Release month'),
+             alt.Tooltip('Country'),
+             alt.Tooltip('POI with polygons', format=',')]
+).properties(
+    width=900,
+    height=500,
+    title=alt.TitleParams(
+        text='Total POI with Polygon Count - Last 12 months',
+        fontSize=18
+    )
+).configure_axisY(
+    labelAngle=20
+).configure_axisX(
+    title=None,
+    labelAngle=45
+)
+
+st.altair_chart(last_12_months_geometry)
