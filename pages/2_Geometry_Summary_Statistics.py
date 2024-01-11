2affde633786945bfd3570cfd957b4c2fb867d85
ilanrich <54144308+ilanrich@users.noreply.github.com>
2024-01-11 13:24:31 -0500

    Add top 30 geometry panel

--- pages/2_Geometry_Summary_Statistics.py
+++ pages/2_Geometry_Summary_Statistics.py
@@ -41,3 +41,32 @@ total_poi = latest_release_df.iloc[-1]["Total POI with Parking Lots"]
 
 st.write(f"POI count across all countries, including parking lots POI is <b>{total_poi:,.0f}</b>", unsafe_allow_html=True)
 st.dataframe(latest_release_df_styled, use_container_width=True)
+
+#### Top 30 Geometry ####
+top_30_geometry_df = (
+    read_from_gsheets("Countries")
+    .assign(**{
+        "Total POI with Parking Lots": lambda df: df["Total POI with Parking Lots"].astype(int),
+        "POI with polygons": lambda df: df["POI with polygons"].str.replace(",", "").astype(int),
+        "Point-only POI": lambda df: df["Point-only POI"].str.replace(",", "").astype(int),
+        "Polygon coverage": lambda df: ((df["Polygon coverage"].str.replace(",", "").astype(float)) * 100).astype(float)
+    })
+    .query('iso_country_code != "US"')
+    .rename(columns={"iso_country_code": "Country Code", "country": "Country"})
+    [["Country Code", "Country", "Total POI with Parking Lots", "POI with polygons", "Point-only POI", "Polygon coverage"]]
+    .head(30)
+)
+
+top_30_df_geometry_styled = (
+    top_30_geometry_df.style
+    .apply(lambda x: ['background-color: #D7E8ED' if i%2==0 else '' for i in range(len(x))], axis=0)
+    .format({
+       "Total POI with Parking Lots": "{:,.0f}",
+        "POI with polygons": "{:,.0f}",
+        "Point-only POI": "{:,.0f}",
+        "Polygon coverage": "{:.01f}%"
+    })
+)
+
+st.write("POI Counts - Top 30 Countries Outside the US")
+st.dataframe(top_30_df_geometry_styled, use_container_width=True)
