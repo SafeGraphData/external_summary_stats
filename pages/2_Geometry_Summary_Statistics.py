3e57956c1ff0235a0e237c7c0868a2e5c9bea9e7
ilanrich <54144308+ilanrich@users.noreply.github.com>
2024-01-12 12:20:16 -0500

    update column name to % of POI with Parking Lot

--- pages/2_Geometry_Summary_Statistics.py
+++ pages/2_Geometry_Summary_Statistics.py
@@ -177,14 +177,14 @@ for i, tab in enumerate(tabs[1:]):
 state_df = read_from_gsheets("Parking - regions").assign(**{
         "total_parking_poi": lambda df: df["total_parking_poi"].astype(int),
         "%_poi_with_parking": lambda df: ((df["pct_poi_with_parking"].astype(float)) * 100).astype(float)
-    })
+    }).rename(columns={"%_poi_with_parking": "% of POI with Parking Lot"})
 
-state_df['%_poi_with_parking'] = [round(x,1) for x in state_df['%_poi_with_parking']]
+state_df['% of POI with Parking Lot'] = [round(x,1) for x in state_df['% of POI with Parking Lot']]
 
 # Create the plot with USA scope, gray background, and tooltips
 fig = px.choropleth(state_df,
                     locations='state',
-                    color='%_poi_with_parking',
+                    color='% of POI with Parking Lot',
                     hover_name='state',
                     locationmode='USA-states',  # Use predefined US state boundaries
                     scope='usa',  # Set the scope to 'usa'
@@ -209,10 +209,10 @@ fig.update_layout(
         orientation='h',  # Set the orientation to horizontal
         x=0,  # Set the x position
         y=1.02,  # Set the y position
-        xanchor='left',  # Set the x anchor to left
+       # xanchor='left',  # Set the x anchor to left
         yanchor='bottom'  # Set the y anchor to bottom
     )
 )
-fig.update_layout(height=800, width=1400) ## Update map size 
+fig.update_layout(height=800, width=1400) 
 st.write('See the map below, where states are shaded based on the percentage of POI that have an associated parking lot POI')
-st.plotly_chart(fig, use_container_width=True)
+st.plotly_chart(fig)
